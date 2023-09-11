7f0479ba783ec97466a2c7c0b350449dc0870599
lumauser <91039519+lumauser@users.noreply.github.com>
2023-09-11 16:24:50 +0000

    New /weather command

--- commands.py
+++ commands.py
@@ -3,6 +3,7 @@ from discord.ext import commands
 from discord import app_commands
 import random
 from discord import Embed
+import requests
 
 def setup_commands(bot: commands.Bot):
     @bot.tree.command(name="hello", description="Says `Hello`")
@@ -48,3 +49,55 @@ def setup_commands(bot: commands.Bot):
       user_mention = f"<@{interaction.user.id}>"
       embed = discord.Embed(description=f"{user_mention} has paid their respects 🫡\n :regional_indicator_f: Use /f to pay respects")
       await interaction.response.send_message(embed=embed)
+    
+    @bot.tree.command(name="weather", description="Get weather information for a city")
+    @app_commands.describe(city="Enter a city name")
+    async def weather(interaction: discord.Interaction, city: str):
+      city = city.title()
+
+      base_url = 'https://api.openweathermap.org/data/2.5/weather'
+      API_KEY = '3ba4e0181d9cd2210a137bb648ee4fc5'
+
+      params = {
+          'q': city,      
+          'appid': API_KEY,
+          'units': 'metric',
+      }
+
+      response = requests.get(base_url, params=params)
+      data = response.json()
+
+      if response.status_code == 200:
+          weather_desc = data['weather'][0]['description']
+          temperature_celsius = data['main']['temp']
+          temperature_fahrenheit = (temperature_celsius * 9/5) + 32
+          humidity = data['main']['humidity']
+
+          weather_emoji = {
+              'clear sky': '☀️',
+              'few clouds': '🌤️',
+              'scattered clouds': '🌥️',
+              'broken clouds': '☁️',
+              'shower rain': '🌦️',
+              'rain': '🌧️',
+              'thunderstorm': '⛈️',
+              'snow': '❄️',
+              'mist': '🌫️',
+              'overcast clouds': '🌁'
+          }
+
+          emoji = weather_emoji.get(weather_desc.lower(), '🤷‍♀️')
+
+          embed = discord.Embed(
+            title=f"Weather in {city}  🌡️",
+            description=f"**Condition:** {weather_desc.capitalize()} {emoji}\n"
+                        f"**Temperature:** {temperature_celsius}°C / {temperature_fahrenheit}°F\n"
+                        f"**Humidity:** {humidity}%\n"
+                        f"Misspelled Cities will not work\n"
+                        f"Weather data provided by OpenWeatherMapAPI",
+            color=discord.Color.blue()
+        )
+
+          await interaction.response.send_message(embed=embed)
+      else:
+          await interaction.response.send_message("Unable to fetch weather information for that city.", ephemeral=True)
